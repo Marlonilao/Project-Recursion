2086de461a47152f0af06c2c326d105719663496
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-08-29 16:05:28 +0800

    wip

---
+++ fibonacci.js
@@ -0,0 +1,18 @@
+// function fibs(n) {
+//   let array = [0, 1];
+//   if (n < 2) {
+//     return array;
+//   }
+//   for (let i = 2; i < n; i++) {
+//     array[i] = array[i - 1] + array[i - 2];
+//   }
+//   return array;
+// }  ** this is the iterative method
+
+function fibsRec(n) {
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const array = fibsRec(n - 1);
+  return array.concat(array[array.length - 1] + array[array.length - 2]);
+}
